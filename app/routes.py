3f541bfe97735c9498dcf49c701e2228bb3a2d35
Stephen Graham <2385583g@student.gla.ac.uk>
2021-02-22 23:58:15 +0000

    added fully functional user authentication

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Steve'}
     posts = [
         {
             'author': {'username': 'Jamie'},
@@ -17,14 +20,51 @@ def index():
             'body': 'Currently learning Flask!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        # Store the request message for each user who is validated
-        flash('Login requested for user {}, rememeber_me= {}'.format(form.username.data, form.remember_me.data))
+    # Check if the user is already logged in
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
+    form = LoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        # Query returns the user if it exists, or None if otherwise
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        # Otherwise successfully login the user
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        # prevents malicious exploitation of the next argument
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    # Add the user to the database if the form is validated
+    if request.method == 'POST' and form.validate_on_submit():
+        user = User(username=form.username.data, email=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You are now successfully registered!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+
