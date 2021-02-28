eb0d7ae906dd7dc2bf2d123c134fbb7ca987af8d
Stephen Graham <2385583g@student.gla.ac.uk>
2021-02-28 15:28:12 +0000

    added flask-bootstrap to app and implemented navbar

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
 
 from config import Config
 
@@ -15,6 +17,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
